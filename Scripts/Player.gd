289bb2b5158afcafb34fed7e1d91dcf1a7c48842
jake6724 <jakealdridge6724@gmail.com>
2025-04-15 23:18:28 -0400

    Continue work on trajectory calc

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,6 +12,12 @@ var speed: float = 500
 var jump: float = -750
 var draw_complete: bool = false
 var max_draw_strength: float = 2000
+var draw_strength: float
+var trajectory_line: Line2D
+
+func _ready():
+	trajectory_line = Line2D.new()
+	add_child(trajectory_line)
 
 func _process(_delta):
 	# Aim bow
@@ -54,21 +60,59 @@ func shoot_arrow():
 	new_arrow.position += d * 100
 	add_child(new_arrow)
 
-	var draw_strength: float
-
-
-	print(bow.frame)
-	print("Length", ap.get_animation(ap.current_animation).length)
-
 	var p = ap.current_animation_position / ap.get_animation(ap.current_animation).length 
-
 
 	if draw_complete:
 		draw_strength = max_draw_strength
 	else:
 		draw_strength = p * max_draw_strength
 
-	print("Animation position: ", ap.current_animation_position)
-	print("Draw Strength: ", draw_strength)
-
+	calc_trajectory()
 	new_arrow.fly(draw_strength, d)
+
+func calc_trajectory():
+	var angle = global_position.angle_to_point(get_global_mouse_position())
+	print("angle: ", -rad_to_deg(angle))
+
+	
+
+
+# func calc_trajectory():
+# 	trajectory_line.clear_points()
+# 	trajectory_line.add_point(to_local(global_position))
+# 	for i in range(10):
+# 		var d = (get_global_mouse_position() - global_position).normalized()
+# 		var angle = atan2(d.y, d.x)
+# 		var x = global_position.x + ((draw_strength * ap.current_animation_position / ap.get_animation(ap.current_animation).length) 
+# 		* cos(angle) * i)
+
+# 		var y = global_position.y + (((draw_strength * ap.current_animation_position / ap.get_animation(ap.current_animation).length) 
+# 		* sin(angle) * i) - .5 * 9.81 * i * i)
+
+# 		var new_point: Vector2 = Vector2(x, y)
+# 		print("New point: ", new_point)
+# 		trajectory_line.add_point(new_point)
+# 		print("Updated TLPs: ", trajectory_line.points)
+# 	print(trajectory_line.points)
+
+# func calc_trajectory():
+# 	print("Calc T")
+
+# 	for i in range(10):
+
+# 		var d = (get_global_mouse_position() - global_position).normalized()
+# 		var angle = atan2(d.y, d.x)
+# 		var x = global_position.x + ((draw_strength * ap.current_animation_position / ap.get_animation(ap.current_animation).length) 
+# 		* cos(angle) * i)
+
+# 		var y = global_position.y + ((draw_strength * ap.current_animation_position / ap.get_animation(ap.current_animation).length) 
+# 		* sin(angle) * i)
+
+		
+# 		var line: Line2D = Line2D.new()
+# 		# add_child(line)
+# 		trajectory_line.points = PackedVector2Array([to_local(global_position), Vector2(x,y)])
+
+# 		print("Line gp: ", line.global_position)
+# 		print("Player gp: ", global_position)
+# 		print("Line points: ", line.points)
